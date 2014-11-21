fff360e1870d5d2c7eecdc1f597647b3a6ab880c
Martin Kiss <martin.kiss@me.com>
2014-11-21 16:54:34 +0100

    Implement Locale source and basic detection

--- Sources/Where.h
+++ Sources/Where.h
@@ -7,12 +7,55 @@
 //
 
 @import UIKit;
+@import CoreLocation;
 
 
-FOUNDATION_EXPORT double WhereVersionNumber;
+FOUNDATION_EXPORT const double WhereVersionNumber;
 FOUNDATION_EXPORT const unsigned char WhereVersionString[];
+
+
+typedef enum : NSUInteger {
+    WhereSourceNone = 0,
+    WhereSourceLocale,      // Instant, not reliable, always.
+    //TODO: WhereSourceTimeZone,    // Instant, reliable, always.
+    //TODO: WhereSourceCarrier,     // Instant, reliable, cellular.
+    //TODO: WhereSourceIPAddress,   // Delayed, reliable, connection.
+    //TODO: WhereSourceLocationServices, // Delayed, precise, permission.
+} WhereSource;
 
 
 @interface Where : NSObject
 
+@property (readonly) WhereSource source;
+@property (readonly) NSString *countryCode;
+@property (readonly) NSString *countryName;
+//TODO: @property (readonly) CLLocationCoordinate2D coordinate;
+@property (readonly) NSDate *timestamp;
+
 @end
+
+
+@interface Where (Detection)
+
+// Instant
++ (void)detectInstantly;
+
+// Asynchronous
+//TODO: + (void)startDetection;
+//TODO: + (BOOL)isDetecting;
+//TODO: + (void)stopDetection;
+
+// Updating
+//TODO: + (BOOL)isUpdating;
+//TODO: + (void)setUpdating:(BOOL)isUpdating;
+
+// Instances
++ (Where *)best;
++ (NSArray *)all;
++ (Where *)forSource:(WhereSource)source;
+
+@end
+
+
+extern NSString * const WhereDidUpdateNotification;
+
--- Sources/Where.m
+++ Sources/Where.m
@@ -9,7 +9,80 @@
 #import "Where.h"
 
 
-@implementation Where
+const double WhereVersionNumber = 1;
+const unsigned char WhereVersionString[] = "1.0.0";
+
+
+@interface Where ()
+
+- (instancetype)initWithSource:(WhereSource)source countryCode:(NSString *)code;
 
 @end
+
+
+@implementation Where
+
+- (instancetype)initWithSource:(WhereSource)source countryCode:(NSString *)code {
+    self = [super init];
+    if (self) {
+        self->_source = source;
+        self->_countryCode = code;
+        NSLocale *posix = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+        self->_countryName = [posix displayNameForKey:NSLocaleCountryCode value:code];
+        self->_timestamp = [NSDate new];
+    }
+    return self;
+}
+
+@end
+
+
+@implementation Where (Detection)
+
++ (void)detectInstantly {
+    NSLog(@"Where are you?");
+    [self detectUsingLocale];
+}
+
++ (void)detectUsingLocale {
+    NSString *country = [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
+    Where *instance = [[Where alloc] initWithSource:WhereSourceLocale countryCode:country];
+    NSLog(@"It seems like you are from %@.", instance.countryName);
+    [self update:instance];
+}
+
++ (void)update:(Where *)instance {
+    NSParameterAssert(instance);
+    if ( ! instance) return;
+    if (instance.source == WhereSourceNone) return;
+    
+    [[self bySource] setObject:instance forKey:@(instance.source)];
+    [[NSNotificationCenter defaultCenter] postNotificationName:WhereDidUpdateNotification object:instance];
+}
+
++ (NSMutableDictionary *)bySource {
+    static NSMutableDictionary *dictionary = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dictionary = [NSMutableDictionary dictionaryWithCapacity:4];
+    });
+    return dictionary;
+}
+
++ (Where *)best {
+    return [self forSource:WhereSourceLocale];
+}
+
++ (NSArray *)all {
+    return [[self bySource] allValues];
+}
+
++ (Where *)forSource:(WhereSource)source {
+    return [[self bySource] objectForKey:@(source)];
+}
+
+@end
+
+
+NSString * const WhereDidUpdateNotification = @"WhereDidUpdateNotification";
 
