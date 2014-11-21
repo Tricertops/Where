7a6812e01db0454bf2270465244d1c4f9d755f54
Martin Kiss <martin.kiss@me.com>
2014-11-21 16:54:17 +0100

    Initial project setup

---
+++ Sources/Where.h
@@ -0,0 +1,18 @@
+//
+//  Where.h
+//  Where
+//
+//  Created by Martin Kiss on 21.11.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+@import UIKit;
+
+
+FOUNDATION_EXPORT double WhereVersionNumber;
+FOUNDATION_EXPORT const unsigned char WhereVersionString[];
+
+
+@interface Where : NSObject
+
+@end
---
+++ Sources/Where.m
@@ -0,0 +1,15 @@
+//
+//  Where.m
+//  Where
+//
+//  Created by Martin Kiss on 21.11.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+#import "Where.h"
+
+
+@implementation Where
+
+@end
+
