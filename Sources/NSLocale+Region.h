fc3a7b8dae00f0a04c6190f419f9c694df67d70c
Martin Kiss <martin.kiss@me.com>
2014-11-23 18:16:08 +0100

    Add map from region ISO code to its coordinates

--- Sources/NSLocale+Region.h
+++ Sources/NSLocale+Region.h
@@ -7,18 +7,38 @@
 //
 
 @import Foundation;
+@import CoreLocation.CLLocation;
 
 
 @interface NSLocale (Region)
 
-//! Takes 3-letter or 2-letter ISO 3166-1 code, returns 2-letter ISO 3166-1 code.
-+ (NSString *)canonizeRegionCode:(NSString *)regionCode;
+
+#pragma mark Standard
 
 //! Returns locale “en_US_POSIX” identifier.
 + (instancetype)standardLocale;
 
+
+#pragma mark Regions
+
+//! The region/country code.
+@property (readonly) NSString *regionCode;
+
+//! Takes 3-letter or 2-letter ISO 3166-1 code, returns 2-letter ISO 3166-1 code.
++ (NSString *)canonizeRegionCode:(NSString *)regionCode;
+
 //! Returns localized name of the region using standardized English locale.
 + (NSString *)nameOfRegion:(NSString *)regionCode;
+
+
+#pragma mark Coordinates
+
+//! Returns middle coordinates for region which this locale represents.
+- (CLLocationCoordinate2D)regionCoordinates;
+
+//! Returns middle coordinates for given region.
++ (CLLocationCoordinate2D)coordinateForRegion:(NSString *)regionCode;
+
 
 @end
 
