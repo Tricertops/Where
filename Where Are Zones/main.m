a5edb3cbdab2376a2c3548073caa3b1b0f38677a
Martin Kiss <martin.kiss@me.com>
2014-11-25 10:18:54 +0100

    Tool for parsing tz database

---
+++ Where Are Zones/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Where Are Zones
+//
+//  Created by Martin Kiss on 25.11.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+@import Foundation;
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        const NSURL *projectDir = [NSURL fileURLWithPath:PROJECT_DIR];
+        
+        
+    }
+    return 0;
+}
+
