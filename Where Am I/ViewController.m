7b0d99141ce48fdce875961cb8a8b213b6ffdd05
Martin Kiss <martin.kiss@me.com>
2014-11-25 13:41:39 +0100

    Display all locations on the map

--- Where Am I/ViewController.m
+++ Where Am I/ViewController.m
@@ -18,8 +18,10 @@
 @property IBOutlet UISwitch *locationSwitch;
 
 @property IBOutlet MKMapView *mapView;
-@property MKPointAnnotation *travelAnnotation;
-@property MKPointAnnotation *homeAnnotation;
+
+@property MKPointAnnotation *source1Annotation;
+@property MKPointAnnotation *source2Annotation;
+@property MKPointAnnotation *source3Annotation;
 
 @property IBOutlet UILabel *source1Title;
 @property IBOutlet UILabel *source2Title;
@@ -41,11 +43,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.travelAnnotation = [MKPointAnnotation new];
-    self.travelAnnotation.title = @"You Are Here";
-    
-    self.homeAnnotation = [MKPointAnnotation new];
-    self.homeAnnotation.title = @"You Live Here";
+    self.source1Annotation = [MKPointAnnotation new];
+    self.source2Annotation = [MKPointAnnotation new];
+    self.source3Annotation = [MKPointAnnotation new];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(update) name:WhereDidUpdateNotification object:nil];
     
@@ -69,42 +69,46 @@
 }
 
 - (IBAction)update {
-    [self updateAnnotation:self.homeAnnotation withLocation:[Where isHome]];
-    [self updateAnnotation:self.travelAnnotation withLocation:[Where amI]];
-    
     BOOL isTravel = (self.segmentControl.selectedSegmentIndex == 0);
-    if (isTravel && [self.mapView.annotations containsObject:self.travelAnnotation]) {
-        [self.mapView selectAnnotation:self.travelAnnotation animated:YES];
-    }
-    if ( ! isTravel && [self.mapView.annotations containsObject:self.homeAnnotation]) {
-        [self.mapView selectAnnotation:self.homeAnnotation animated:YES];
-    }
-    
     WhereSource source1 = (isTravel? WhereSourceWiFiIPAddress : WhereSourceLocale);
     WhereSource source2 = (isTravel? WhereSourceTimeZone : WhereSourceCarrier);
     WhereSource source3 = (isTravel? WhereSourceLocationServices : WhereSourceCellularIPAddress);
+    
+    Where *location1 = [Where forSource:source1];
+    Where *location2 = [Where forSource:source2];
+    Where *location3 = [Where forSource:source3];
+    
+    [self updateAnnotation:self.source1Annotation withLocation:location1];
+    [self updateAnnotation:self.source2Annotation withLocation:location2];
+    [self updateAnnotation:self.source3Annotation withLocation:location3];
+    
+    NSArray *annotations = @[ self.source3Annotation, self.source2Annotation, self.source1Annotation ];
+    MKPointAnnotation *selected = [annotations firstObjectCommonWithArray:self.mapView.annotations];
+    if (selected) {
+        [self.mapView selectAnnotation:selected animated:YES];
+    }
     
     self.source1Title.text = [self sourceTitle:source1];
     self.source2Title.text = [self sourceTitle:source2];
     self.source3Title.text = [self sourceTitle:source3];
     
-    self.source1Region.text = [[Where forSource:source1] regionName];
-    self.source2Region.text = [[Where forSource:source2] regionName];
-    self.source3Region.text = [[Where forSource:source3] regionName];
+    self.source1Region.text = location1.regionName;
+    self.source2Region.text = location2.regionName;
+    self.source3Region.text = location3.regionName;
     
-    self.source1Code.text = [[Where forSource:source1] regionCode];
-    self.source2Code.text = [[Where forSource:source2] regionCode];
-    self.source3Code.text = [[Where forSource:source3] regionCode];
+    self.source1Code.text = location1.regionCode;
+    self.source2Code.text = location2.regionCode;
+    self.source3Code.text = location3.regionCode;
 }
 
 - (void)updateAnnotation:(MKPointAnnotation *)annotation withLocation:(Where *)location {
+    [self.mapView removeAnnotation:annotation];
+    
+    annotation.title = [self sourceTitle:location.source];
     annotation.subtitle = location.regionName;
     annotation.coordinate = location.coordinate;
     if (location) {
         [self.mapView addAnnotation:annotation];
-    }
-    else {
-        [self.mapView removeAnnotation:annotation];
     }
 }
 
